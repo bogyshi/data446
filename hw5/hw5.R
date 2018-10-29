24b88f8b7ddd1823b60b694590be3a74f6e5912b
bogyshi <avanroi1@binghamton.edu>
2018-10-28 23:08:18 -0700

    up to 4

--- hw5/hw5.R
+++ hw5/hw5.R
@@ -7,25 +7,68 @@ library("data.table", lib.loc="~/R/win-library/3.5")
 library("MLmetrics", lib.loc="~/R/win-library/3.5")
 library(reshape2)
 
-binUnif= function(n,a,b)
+binUnif= function(n,a,b,intv,mfactor)
 {
+  message = paste("From",a)
+  message = paste(message,"to")
+  message = paste(message,b)
+  print(message)
   resultsog = runif(n,a,b)
-  results = floor(resultsog*10)
+  results = floor(resultsog*mfactor)
   data = data.frame(results)
-  vals = seq(a,b,.1)
+  vals = seq(a,b,intv)
+  pdf(paste(b,"graph.pdf",sep="")) 
   p<-ggplot(data=data, aes(x=factor(data[,1]))) +geom_bar(stat="count") + scale_x_discrete(labels=vals)
-  p
+  print(p)
+  dev.off()
   #barplot(results,main="whatev",width=0.5)
   print(median(resultsog))
+  getmode = table(resultsog)
+  print(which.max(getmode))
   #print(forMode[c(1:10),])
 }
-
+expoMedMode = function(n,rate)
+{
+  resultsog = rexp(n,rate=rate)
+  results = floor(resultsog*10)
+  data = data.frame(results)
+  data = data.frame(data[data[,1]<25,])
+  vals = seq(0,2.5,.1)
+  p<-ggplot(data=data, aes(x=factor(data[,1]))) +geom_bar(stat="count") + scale_x_discrete(labels=vals)
+  print(p)
+  #barplot(results,main="whatev",width=0.5)
+  print(median(resultsog))
+  getmode = table(resultsog)
+  print(which.max(getmode))
+}
 
 #this is 1a
 set.seed(123)
-n=100000
-binUnif(n,0,2)
+n=1000000
+binUnif(n,0,2,0.1,10)
 
+#this is 2a
+set.seed(123)
+n=1000000
+expoMedMode(n,2)
+
+#this is 3a
+set.seed(433)
+counter = 1
+n=10
+size = 1000
+while(counter <= n)
+{
+  binUnif(size,1,counter,1,1)
+  counter = counter + 1
+}
+
+#this is 3b
+?dunif
+oddNum = data.frame(c(.2,.4,.6,.8,1.0),c(1,2,3,4,5))
+p<-ggplot(data=oddNum, aes(x=oddNum[,2],y=oddNum[,1])) + geom_bar(stat="identity")
+p
+punif(1.5,1,2)
 results = runif(n,0,1)
 counter = 1
 areas=numeric(0)
